a097aeb305ed93a2df4c6c8fbb978688612ebe7b
Joel <joelthomas6094@gmail.com>
2024-04-08 10:57:13 +0530

    bug12

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,10 @@ import "./globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
+  variable: '--body-font',
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
+
 
 export const metadata = {
   title: "Create Next App",
