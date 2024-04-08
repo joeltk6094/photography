90f7cdbc53d658be70f503af48085c1d0dc4de75
Joel <joelthomas6094@gmail.com>
2024-04-08 10:06:04 +0530

    bug10

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,11 @@ import { Poppins } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 
-const poppins = Poppins({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"], 
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] 
+});
+
 
 export const metadata = {
   title: "Create Next App",
